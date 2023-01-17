9c2056935c280b4e7f93f189aaa120af41b9114d
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2023-01-17 19:00:00 -0300

    booklist

---
+++ booklist/src/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg"
+    alt="Interesting Facts For Curious Minds"
+  />
+);
+const Title = () => {
+  return <h2>Interesting Facts For Curious Minds</h2>;
+};
+const Author = () => <h4>Jordan Moore </h4>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
