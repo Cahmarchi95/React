097ddf8ff67d37e85eb4b8ae5068b52d91befd50
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2023-01-18 15:35:29 -0300

    learning props

--- booklist/src/index.js
+++ booklist/src/index.js
@@ -1,38 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css"
+import "./index.css";
+
+const firstBook = {
+  author: "Jordan Moore",
+  title: "Interesting Facts For Curious Minds",
+  img: "./images/book-1.jpg",
+};
+const secondBook = {
+  author: " Colleen Hoover",
+  title: "It Starts with Us",
+  img: "https://m.media-amazon.com/images/I/51VC+Vru96L._SY344_BO1,204,203,200_.jpg",
+};
+
+const thirdBook = {
+  author: "Rick Rubin ",
+  title: "The Creative Act: A Way of Being",
+  img: "https://m.media-amazon.com/images/I/41AkIg2fDcL._SX339_BO1,204,203,200_.jpg",
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
+      <Book
+        author={thirdBook.author}
+        title={thirdBook.title}
+        img={thirdBook.img}
+      />
     </section>
   );
 }
-
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author} </h4>
     </article>
   );
 };
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg"
-    alt="Interesting Facts For Curious Minds"
-  />
-);
-const Title = () => {
-  return <h2>Interesting Facts For Curious Minds</h2>;
-};
-const Author = () => <h4>Jordan Moore </h4>;
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<BookList />);
