dc4758d29428bfde729a7ebfaddac23f4b8859d9
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2023-01-26 20:18:30 -0300

    backroad app

---
+++ backroads-app/src/components/Navbar.js
@@ -0,0 +1,46 @@
+import logo from "../images/logo.svg";
+import { pageLinks, socialLinks } from "../data";
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        {/*<!-- left this comment on purpose -->*/}
+        <ul className="nav-links" id="nav-links">
+          {pageLinks.map(({ href, text, id }) => {
+            return (
+              <li key={id}>
+                <a href={href} className="nav-link">
+                  {text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+        <ul className="nav-icons">
+          {socialLinks.map(({ href, id, icon }) => {
+            return (
+              <li key={id}>
+                <a
+                  href={href}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="nav-icon"
+                >
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
