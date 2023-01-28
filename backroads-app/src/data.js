5d6745d631ea0993e6b36697b711344def1a8fd9
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2023-01-27 22:18:04 -0300

    making components

--- backroads-app/src/data.js
+++ backroads-app/src/data.js
@@ -1,3 +1,9 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+import tour5 from "./images/tour-5.jpeg";
+
 export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
@@ -5,9 +11,84 @@ export const pageLinks = [
   { id: 4, href: "#tours", text: "tours" },
 ];
 
-
 export const socialLinks = [
   { id: 1, href: "https://www.facebook.com", icon: "fab fa-facebook" },
   { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
   { id: 3, href: "https://pt.squarespace.com/", icon: "fab fa-squarespace" },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    img: tour1,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    place: "china",
+    days: "6 days",
+    cost: "from $2100",
+  },
+  {
+    id: 2,
+    img: tour2,
+    date: "october 1th, 2020",
+    title: "best of java",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    place: "indonesia",
+    days: "11 days",
+    cost: "from $1400",
+  },
+
+  {
+    id: 3,
+    img: tour3,
+    date: "september 15th, 2020",
+    title: "explore hong kong",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    place: "hong kong",
+    days: "8 days",
+    cost: "from $5000",
+  },
+
+  {
+    id: 4,
+    img: tour4,
+    date: "december 5th, 2019",
+    title: "kenya highlights",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    place: "kenya",
+    days: "20 days",
+    cost: "from $3300",
+  },
+  {
+    id: 5,
+    img: tour5,
+    date: "december 5th, 2019",
+    title: "safari in africa",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    place: "africa",
+    days: "4 days",
+    cost: "from $5500",
+  },
 ];
