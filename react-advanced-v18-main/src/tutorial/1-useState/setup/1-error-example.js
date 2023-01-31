9501b2176d1c223ccfb60c3ee67bdb62d68dcddb
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2023-01-30 21:01:53 -0300

    useState tutorial

---
+++ react-advanced-v18-main/src/tutorial/1-useState/setup/1-error-example.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ErrorExample = () => {
+  let title = "random title"
+  const handleClick = () =>{
+    title = "hello people"
+    console.log(title);
+  }
+  return <>
+  <h2>{title}</h2>
+  <button type='button' className='btn' onClick={handleClick}>change title</button>
+  </>
+};
+
+export default ErrorExample;
