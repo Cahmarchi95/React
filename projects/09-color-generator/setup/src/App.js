01e55b6c1c27ff5b30f16a5188bd44c3d1ea73d6
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2023-02-20 18:39:26 -0300

    color generator project

---
+++ projects/09-color-generator/setup/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+
+import Values from "values.js";
+
+function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#FFEE01").all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+      setError(false);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#FFEE01"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      ;
+      <section className="colors">
+        {list.map((color, index) => {
+          console.log(color);
+          return <SingleColor key={index} {...color} index={index} />;
+        })}
+      </section>
+    </>
+  );
+}
+export default App;
