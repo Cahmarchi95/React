7e07b19239bccd9ffe6572ac8f0dc9a570479cca
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2023-02-25 18:53:27 -0300

    global context

---
+++ 03-advanced-react/src/tutorial/global-context/src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import AppContext from "./context";
+import "./index.css";
+AppContext;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AppContext>
+      <App />
+    </AppContext>
+  </React.StrictMode>
+);
