59a74e41d95d9315b838de95686dab49c0bfe6af
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2023-03-02 19:04:58 -0300

    stripe project complete

--- projects/13-stripe-submenus/setup/src/Navbar.js
+++ projects/13-stripe-submenus/setup/src/Navbar.js
@@ -10,12 +10,18 @@ const Navbar = () => {
     const page = e.target.textContent;
     const tempBtn = e.target.getBoundingClientRect();
     const center = (tempBtn.left + tempBtn.right) / 2;
-    const bottom = tempBtn.bottom - 3
-    openSubmenu(page,{center,bottom});
+    const bottom = tempBtn.bottom - 3;
+    openSubmenu(page, { center, bottom });
+  };
+
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains("link-btn")) {
+      closeSubmenu();
+    }
   };
 
   return (
-    <nav className="nav">
+    <nav className="nav" onMouseOver={handleSubmenu}>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className="nav-logo" alt="stripe" />
--- projects/13-stripe-submenus/setup/src/context.js
+++ projects/13-stripe-submenus/setup/src/context.js
@@ -7,6 +7,7 @@ export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
   const [location, setLocation] = useState({});
+  const [page,setPage] = useState({page:"",links:[]})
 
   const openSidebar = () => {
     setIsSidebarOpen(true);
@@ -17,6 +18,8 @@ export const AppProvider = ({ children }) => {
   };
 
   const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text)
+    setPage(page)
     setLocation(coordinates);
     setIsSubmenuOpen(true);
   };
@@ -35,6 +38,7 @@ export const AppProvider = ({ children }) => {
         closeSubmenu,
         closeSidebar,
         location,
+        page,
       }}
     >
       {children}
