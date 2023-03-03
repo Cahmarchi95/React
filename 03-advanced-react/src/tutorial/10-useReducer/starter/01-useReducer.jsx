8874ab9e5a68bc8541ef49ba75d67dedd62873d3
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2023-03-03 20:40:07 -0300

    useReducer tutorial

--- 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,34 +1,32 @@
 import { useState, useReducer } from "react";
-import { data } from "../../../data";
+import { data, people } from "../../../data";
+import { CLEAR_LIST, RESET_LIST, REMOVE_ITEM } from "./actions";
+import reducer from "./reducer";
 
 const defaultState = {
-  people:data,
-}
-
-const reducer = ()=>{
-
-}
+  people: data,
+  isLoading: false,
+};
 
 const ReducerBasics = () => {
-
-  const [state,dispatch] = useReducer(reducer,defaultState)
-
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
+    dispatch({ type: REMOVE_ITEM, payload: { id } });
     // let newPeople = people.filter((person) => person.id !== id);
     // setPeople(newPeople);
   };
 
-  
   const clearList = () => {
+    dispatch({ type: CLEAR_LIST });
     // setPeople([]);
   };
 
-
   const resetList = () => {
+    dispatch({ type: RESET_LIST });
     // setPeople(data);
   };
-console.log(state);
+  console.log(state);
   return (
     <div className="container">
       {state.people.map((person) => {
