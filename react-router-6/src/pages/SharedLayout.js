09443ab5c082dc59a942456c00e7782b50c55ab8
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2023-03-10 15:30:48 -0300

    router tutorial

---
+++ react-router-6/src/pages/SharedLayout.js
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+const SharedLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+export default SharedLayout;
