9fa09730b6109051bb04e72c344449628b835994
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2023-03-12 19:56:07 -0300

    react router tutorial

--- react-router-6/src/App.js
+++ react-router-6/src/App.js
@@ -4,6 +4,8 @@ import About from "./pages/About";
 import Products from "./pages/Products";
 import Error from "./pages/Error";
 import SharedLayout from "./pages/SharedLayout";
+import SingleProduct from "./pages/SingleProduct";
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="products" element={<Products />} />
+          <Route path="products/:productId" element={<SingleProduct/>}/>
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
--- react-router-6/src/pages/SharedLayout.js
+++ react-router-6/src/pages/SharedLayout.js
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import StyledNavbar from "../components/StyledNavbar";
 
 const SharedLayout = () => {
   return (
     <>
-      <Navbar />
+      <StyledNavbar />
       <Outlet />
     </>
   );
