dd114ce5694f6e3cd19d274abd646f4923a54ae1
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2023-03-29 17:42:35 -0300

    cocktails project

---
+++ projects/15-cocktails/setup/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus sed mollitia eius! Fuga, at soluta itaque exercitationem beatae fugiat sunt tempore asperiores quae veritatis aliquam perspiciatis totam incidunt ab officiis?
+      </p>
+    </section>
+  )
+}
+
+export default About
