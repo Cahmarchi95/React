427644eef4d66b0d7507cf2e02898070d37518f5
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2023-03-28 22:22:13 -0300

    axios tutorial

---
+++ axios-tutorial/src/App.js
@@ -0,0 +1,13 @@
+import Title from './components/Title';
+import Setup from "./examples/6-interceptors"
+
+function App() {
+  return (
+    <main>
+      <Title />
+      <Setup/>
+    </main>
+  );
+}
+
+export default App;
